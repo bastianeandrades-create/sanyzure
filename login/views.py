550e42c9ab3ffeb8b8542c689870f2619e05caa6
bastianeandrades-create <bastian.eandrades@gmail.com>
2025-10-27 10:25:24 -0300

    primer commit

---
+++ login/views.py
@@ -0,0 +1,28 @@
+from django.http import HttpResponse
+from django.shortcuts import render,redirect
+from django.contrib.auth import authenticate,login
+from .forms import LoginForm
+
+def user_login(request):
+    if request.method=='POST':
+        form=LoginForm(request.POST)
+        if form.is_valid():
+            cd=form.cleaned_data
+            user=authenticate(request,
+                              username=cd['username'],
+                              password=cd['password'])
+            if user is not None:
+                if user.is_active:
+                    login(request,user)
+                    return redirect('menu')
+                else:
+                    return HttpResponse('Invalid login')
+    else:
+        form=LoginForm()
+    return render(request,'login.html',{'form':form})
+
+
+
+
+
+   
